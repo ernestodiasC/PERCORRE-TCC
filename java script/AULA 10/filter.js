9f26a33500c9eca5104989745bf2de48f36d0d1c
Moroni <dieivian8@gmail.com>
2026-06-17 13:49:44 -0300

    Moroni

---
+++ java script/AULA 10/filter.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+
+
+
+
+
+// let idadeAlunos = [17,17,16,16,18,16,19,30]
+
+// let maiores = idadeAlunos.filter((idade) => {
+//     return  idade > 17
+
+
+// })
+// console.log(idadeAlunos);
+// console.log(maiores);
+let Alunos = ["Allan","Marcos","João","Maria Klara com K","Isa"]
+
+let nomeMaior  = Alunos.map((Nomes) =>{
+     return Nomes.toUpperCase()
+})
+console.log(Alunos);
+console.log(nomeMaior);
---
+++ java script/aula-02/main.js
@@ -0,0 +1,12 @@
+// let | var | const
+let n1, n2, media, nome 
+
+n1 = 8
+n2 = 9
+media = 8
+nome = "eu"
+
+//  " " | ' ' | ``
+
+console.log ("a nota da" + nome + "é :" + media);
+console.log (`a nota ${nome} é : ${media}`);
