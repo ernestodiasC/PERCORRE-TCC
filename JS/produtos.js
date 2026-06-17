66f289b94fcf116667dc437664aa6b03ec9b37ef
Ernesto Dias Cecilio <ernestodiascecilio@gmail.com>
2026-06-17 16:53:27 -0300

    Adicionando mais rações

--- JS/produtos.js
+++ JS/produtos.js
@@ -5,16 +5,17 @@ class Produto{
         this.img=img
         this.preco=preco
         this.preco_number=Number(preco)
-        this.id=img
+        this.idx=img
 
-        G.PRODUTOS[this.id]=this
+        G.PRODUTOS[this.idx]=this
     }
 }
 class Racao extends Produto{
-    constructor(nome,img,preco,peso,animal){
+    constructor(nome,img,preco,peso,animal,sabor){
         super(nome,"Ração",img,preco)
         this.peso=peso
         this.animal=animal
+        this.sabor=sabor
     }
 }
 class Tulha extends Produto{
@@ -40,8 +41,11 @@ G.F.get_produto_by_id=function(id){
 
 // RAÇÕES
 // Caes
-let rGoldenEspecial=new Racao("Golden Especial - Frango e Carne","racaogolden.jpg","129,99","15kg","Cachorro")
+let rGoldenEspecial=new Racao("Golden Especial - Frango e Carne","racaogolden.jpg","129,99","15kg","Cachorro",["Frango","Carne"])
 let rPremieRRacasGrandes=new Racao("PremieR - Raças Grandes","premieRracasgrandes.png","289,99","15kg","Cachorro")
+
+let rPedigree=new Racao("Pedigree - Adulto","pedigree_adulto_carne_frango_e_cereais_10,1kg.png","289,99","15kg","Cachorro",["Carne","Frango","Cereais"])
+
 // Gatos
-let rWhiskasFrango200g=new Racao("Whiskas - Frango","whiskas200g.png","27.99","900g","Gato")
+let rWhiskasFrango200g=new Racao("Whiskas - Frango","whiskas200g.png","27.99","900g","Gato",["Frango"])
 console.log(G.PRODUTOS)
