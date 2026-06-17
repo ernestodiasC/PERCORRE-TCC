b51d236efb3faa6204ec64bdd22c95aaa8baec76
Ernesto Dias Cecilio <ernestodiascecilio@gmail.com>
2026-06-17 13:55:32 -0300

    main.js mudancas

--- JS/main.js
+++ JS/main.js
@@ -7,7 +7,7 @@ var G={
       if(quant<1){quant=1}
       elemento.innerText=quant
     },
-    // Obtido de https://gist.github.com/tomerof/54c30b25556a902b0ffe22528b48e6e8 feito por tomerof
+    // Versão original obtido de https://gist.github.com/tomerof/54c30b25556a902b0ffe22528b48e6e8 feito por tomerof
     loadScript:function(url,condition,callback){
       if(condition){
         var script=document.createElement('script');
